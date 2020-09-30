e59ce4a1c72b19a8f3d4ffbea202963919c2b34f
marcoelidelara <50976466+marcoelidelara@users.noreply.github.com>
2020-10-01 04:12:24 +0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,2 +1,2 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1GUHkjxv3mZlDZzKA--wgwhbAmSKG5GI4utmNBAQnFJM/edit#gid=0';
-var googleApiKey = 'AIzaSyA9vid-eOEpgKS1l_tLxc8O-4TpdRdFBZ4'
+var googleApiKey = 'AIzaSyA9vid-eOEpgKS1l_tLxc8O-4TpdRdFBZ4';
